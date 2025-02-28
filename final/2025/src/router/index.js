0284913526febeab842b4a3adcc82610e25ea1da
CC Tung <maple51327@gmail.com>
2025-02-28 18:50:05 +0800

    feat(router): scrollBehavior

--- final/2025/src/router/index.js
+++ final/2025/src/router/index.js
@@ -14,6 +14,13 @@ const router = createRouter({
       component: () => import('@/views/MapView.vue'),
     },
   ],
+  scrollBehavior(to, from, savedPosition) {
+    if (savedPosition) {
+      return savedPosition
+    } else {
+      return { top: 0 }
+    }
+  },
 })
 
 export default router
