f0cf194e034ae4c72f4787fc6cea0eeb0b5a1595
creticulata <maple51327@gmail.com>
2025-04-06 16:13:18 +0800

    test 1.3.4

--- final/2025/src/App.vue
+++ final/2025/src/App.vue
@@ -11,7 +11,7 @@ const noteStore = useNoteStore()
 const userStore = useUserStore()
 const router = useRouter()
 const drawer = ref(false)
-const version = ref('1.3.3')
+const version = ref('1.3.4')
 
 function initialize() {
   if (localStorage.getItem('Tomato-key')) {
